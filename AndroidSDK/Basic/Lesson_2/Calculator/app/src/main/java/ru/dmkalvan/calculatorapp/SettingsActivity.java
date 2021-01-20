7c60530fe1f641348b6733763366640395e12290
Lokni <dmitrikalvan@gmail.com>
2021-01-20 20:06:04 +0200

    Add switch listener in SettingsActivity

--- AndroidSDK/Basic/Lesson_2/Calculator/app/src/main/java/ru/dmkalvan/calculatorapp/SettingsActivity.java
+++ AndroidSDK/Basic/Lesson_2/Calculator/app/src/main/java/ru/dmkalvan/calculatorapp/SettingsActivity.java
@@ -1,8 +1,10 @@
 package ru.dmkalvan.calculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        SwitchCompat theme = findViewById(R.id.theme_switch_btn);
+        theme.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked){
+                setTheme(R.style.dark_theme);
+            } else {
+                setTheme(R.style.light_theme);
+            }
+        });
     }
 }
