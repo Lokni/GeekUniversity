e5a058267b78fbdd29c98134b3067f02f5bd85b8
Lokni <dmitrikalvan@gmail.com>
2021-01-21 21:59:59 +0200

    add method finish for close SettingsActivity

--- AndroidSDK/Basic/Lesson_2/Calculator/app/src/main/java/ru/dmkalvan/calculatorapp/SettingsActivity.java
+++ AndroidSDK/Basic/Lesson_2/Calculator/app/src/main/java/ru/dmkalvan/calculatorapp/SettingsActivity.java
@@ -1,8 +1,10 @@
 package ru.dmkalvan.calculatorapp;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +25,10 @@ public class SettingsActivity extends AppCompatActivity {
         setTheme(getAppTheme(R.style.dark_theme));
         setContentView(R.layout.activity_settings);
         initThemeChooser();
+        Button btnOk =  findViewById(R.id.accept_theme);
+        btnOk.setOnClickListener(v ->{
+            finish();
+        });
     }
 
     private void initThemeChooser() {
