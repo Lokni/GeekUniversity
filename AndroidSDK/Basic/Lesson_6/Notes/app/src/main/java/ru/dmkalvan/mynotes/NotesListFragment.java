3d42ece6fd54229fa9ed78538f60f505e8e027ab
Lokni <dmitrikalvan@gmail.com>
2021-01-23 16:51:17 +0200

    Add list with example notes in fragment and show them on screen, made data class StructureData, add dimensions for text size.

--- AndroidSDK/Basic/Lesson_6/Notes/app/src/main/java/ru/dmkalvan/mynotes/NotesListFragment.java
+++ AndroidSDK/Basic/Lesson_6/Notes/app/src/main/java/ru/dmkalvan/mynotes/NotesListFragment.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class NotesListFragment extends Fragment {
@@ -30,5 +34,25 @@ public class NotesListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_notes_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initNotesList(view);
+    }
+
+    //    Create notes list on the screen.
+    private void initNotesList(View view) {
+        LinearLayout linearView = (LinearLayout) view;
+        String[] notes = getResources().getStringArray(R.array.notes_example);
+
+        for (int i = 0; i < notes.length; i++) {
+            String note = notes[i];
+            TextView textView = new TextView(getContext());
+            textView.setText(note);
+            textView.setTextSize(30f);
+            linearView.addView(textView);
+        }
     }
 }
---
+++ AndroidSDK/Basic/Lesson_6/Notes/app/src/main/java/ru/dmkalvan/mynotes/StructureData.java
@@ -0,0 +1,8 @@
+package ru.dmkalvan.mynotes;
+
+class StructureData {
+    String noteLabel;
+    String noteDescription;
+    String noteDate;
+    String noteBody;
+}
