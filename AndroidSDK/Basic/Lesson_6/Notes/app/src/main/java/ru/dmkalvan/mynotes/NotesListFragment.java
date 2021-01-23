c51e822179630eb9c5274d28672c16167a24f47a
Lokni <dmitrikalvan@gmail.com>
2021-01-23 14:59:30 +0200

    Add tasks and empty fragment for notes list.

---
+++ AndroidSDK/Basic/Lesson_6/Notes/app/src/main/java/ru/dmkalvan/mynotes/NotesListFragment.java
@@ -0,0 +1,34 @@
+package ru.dmkalvan.mynotes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class NotesListFragment extends Fragment {
+
+    public NotesListFragment() {
+        // Required empty public constructor
+    }
+
+    public static NotesListFragment newInstance() {
+        NotesListFragment fragment = new NotesListFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notes_list, container, false);
+    }
+}
