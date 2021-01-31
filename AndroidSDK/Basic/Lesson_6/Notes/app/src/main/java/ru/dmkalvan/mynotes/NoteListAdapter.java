78a044d9ca3b888dbbcc7f69c6efc652e67854ba
Lokni <dmitrikalvan@gmail.com>
2021-01-31 19:10:23 +0200

    Add adapter for Recycler view

--- AndroidSDK/Basic/Lesson_6/Notes/app/src/main/java/ru/dmkalvan/mynotes/NoteListAdapter.java
+++ AndroidSDK/Basic/Lesson_6/Notes/app/src/main/java/ru/dmkalvan/mynotes/NoteListAdapter.java
@@ -1,4 +1,52 @@
 package ru.dmkalvan.mynotes;
 
-public class NoteListAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NoteListAdapter extends RecyclerView.Adapter<NoteListAdapter.ViewHolder> {
+
+    private final String[] dataSource;
+
+    public NoteListAdapter(String[] dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @NonNull
+    @Override
+    public NoteListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item, viewGroup, false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteListAdapter.ViewHolder holder, int position) {
+        holder.getTextView().setText(dataSource[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSource.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = (TextView) itemView;
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
 }
