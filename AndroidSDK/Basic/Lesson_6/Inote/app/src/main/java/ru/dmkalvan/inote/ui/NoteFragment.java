dab80927a4ab18d324ea1d4ac82c77d4b9f4b057
Lokni <dmitrikalvan@gmail.com>
2021-02-07 20:14:31 +0200

    Complete UI package

--- AndroidSDK/Basic/Lesson_6/Inote/app/src/main/java/ru/dmkalvan/inote/ui/NoteFragment.java
+++ AndroidSDK/Basic/Lesson_6/Inote/app/src/main/java/ru/dmkalvan/inote/ui/NoteFragment.java
@@ -1,6 +1,139 @@
 package ru.dmkalvan.inote.ui;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import ru.dmkalvan.inote.MainActivity;
+import ru.dmkalvan.inote.R;
+import ru.dmkalvan.inote.data.NoteData;
+import ru.dmkalvan.inote.observe.Publisher;
+
 public class NoteFragment extends Fragment {
+
+    private static final String ARG_NOTE_DATA = "Param_NoteData";
+    private static final String DEFAULT_PARAMS = "Empty";
+
+    private NoteData noteData;
+    private Publisher publisher;
+
+    private TextInputEditText title;
+    private TextInputEditText description;
+    private DatePicker datePicker;
+    private TextInputEditText body;
+
+
+    public static NoteFragment newInstance(NoteData noteData) {
+        NoteFragment fragment = new NoteFragment();
+        Bundle args = new Bundle();
+        args.putParcelable(ARG_NOTE_DATA, noteData);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public static NoteFragment newInstance() {
+        return new NoteFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            noteData = getArguments().getParcelable(ARG_NOTE_DATA);
+        }
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        MainActivity activity = (MainActivity) context;
+        publisher = activity.getPublisher();
+    }
+
+    @Override
+    public void onDetach() {
+        publisher = null;
+        super.onDetach();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_note, container, false);
+        initView(view);
+        // если cardData пустая, то это добавление
+        if (noteData != null) {
+            populateView();
+        }
+        return view;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        noteData = collectorNoteData();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        publisher.notifySingle(noteData);
+    }
+
+    private NoteData collectorNoteData() {
+        String title = this.title.getText().toString();
+        String description = this.description.getText().toString();
+        Date date = getDateFromDatePicker();
+        String body = this.body.getText().toString();
+        if (noteData != null) {
+            NoteData answer;
+            answer = new NoteData(title, description, date, body);
+            answer.setId(noteData.getId());
+            return answer;
+        } else {
+            return new NoteData(DEFAULT_PARAMS, DEFAULT_PARAMS, date, DEFAULT_PARAMS);
+        }
+    }
+
+    private Date getDateFromDatePicker() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, this.datePicker.getYear());
+        cal.set(Calendar.MONTH, this.datePicker.getMonth());
+        cal.set(Calendar.DAY_OF_MONTH, this.datePicker.getDayOfMonth());
+        return cal.getTime();
+    }
+
+    private void initView(View view) {
+        title = view.findViewById(R.id.input_title);
+        description = view.findViewById(R.id.input_description);
+        datePicker = view.findViewById(R.id.input_date);
+        body = view.findViewById(R.id.input_body);
+    }
+
+    private void populateView() {
+        title.setText(noteData.getTitle());
+        description.setText(noteData.getDescription());
+        initDatePicker(noteData.getDate());
+        body.setText(noteData.getBody());
+    }
+
+    private void initDatePicker(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        this.datePicker.init(calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH),
+                null);
+    }
 }
---
+++ AndroidSDK/Basic/Lesson_6/Inote/app/src/main/java/ru/dmkalvan/inote/ui/NoteListAdapter.java
@@ -0,0 +1,120 @@
+package ru.dmkalvan.inote.ui;
+
+import android.annotation.SuppressLint;
+import android.icu.text.SimpleDateFormat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import ru.dmkalvan.inote.R;
+import ru.dmkalvan.inote.data.NoteData;
+import ru.dmkalvan.inote.data.NoteSource;
+
+public class NoteListAdapter extends RecyclerView.Adapter<NoteListAdapter.ViewHolder> {
+    private static final int X_POS = 10;
+    private static final int Y_POS = 10;
+
+
+    private static final String TAG = "SocialNetworkAdapter";
+    private final Fragment fragment;
+    private NoteSource dataSource;
+    private OnItemClickListener itemClickListener;
+    private int menuPosition;
+
+    public NoteListAdapter(Fragment fragment) {
+        this.fragment = fragment;
+    }
+
+    public void setDataSource(NoteSource dataSource) {
+        this.dataSource = dataSource;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item, parent, false);
+        Log.d(TAG, "onCreateViewHolder");
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.setData(dataSource.getNoteData(position));
+        Log.d(TAG, "onBindViewHolder");
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSource.size();
+    }
+
+    public int getMenuPosition() {
+        return menuPosition;
+    }
+
+    public void SetOnItemClickListener(OnItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView title;
+        private final TextView description;
+        private final TextView date;
+        private final LinearLayout card;
+
+        public ViewHolder(@NonNull final View itemView) {
+            super(itemView);
+            card = itemView.findViewById(R.id.note_card);
+            title = itemView.findViewById(R.id.title);
+            description = itemView.findViewById(R.id.description);
+            date = itemView.findViewById(R.id.date);
+
+            registerContextMenu(itemView);
+
+            card.setOnClickListener(v -> {
+                if (itemClickListener != null) {
+                    itemClickListener.onItemClick(v, getAdapterPosition());
+                }
+            });
+
+            card.setOnLongClickListener(v -> {
+                menuPosition = getLayoutPosition();
+                itemView.showContextMenu(X_POS, Y_POS);
+                return true;
+            });
+        }
+
+        private void registerContextMenu(View itemView) {
+            if (fragment != null) {
+                itemView.setOnLongClickListener(v -> {
+                    menuPosition = getLayoutPosition();
+                    return false;
+                });
+                fragment.registerForContextMenu(itemView);
+            }
+        }
+
+        @SuppressLint("SimpleDateFormat")
+        public void setData(NoteData noteData) {
+            title.setText(noteData.getTitle());
+            description.setText(noteData.getDescription());
+            date.setText(new SimpleDateFormat("dd-MM-yy").format(noteData.getDate()));
+        }
+    }
+}
