018a65e1a4cf21a6636205aab9af678d131f5ca4
Lokni <dmitrikalvan@gmail.com>
2021-02-21 16:34:25 +0200

    Connect Kotlin to this project and change MainActivity from java to Kotlin

--- AndroidSDK/Kotlin/Lesson_1/app/src/main/java/ru/dmkalvan/weatherapp/MainActivity.kt
+++ AndroidSDK/Kotlin/Lesson_1/app/src/main/java/ru/dmkalvan/weatherapp/MainActivity.kt
@@ -1,14 +1,12 @@
-package ru.dmkalvan.weatherapp;
+package ru.dmkalvan.weatherapp
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.dmkalvan.weatherapp.R
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
