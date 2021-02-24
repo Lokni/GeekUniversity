9a763ae640c73b99444e943f924a2eac0a491983
Lokni <dmitrikalvan@gmail.com>
2021-02-24 19:17:39 +0200

    Create fragment_weather_card.xml template

---
+++ AndroidSDK/Kotlin/Lesson_1/app/src/main/java/ru/dmkalvan/weatherapp/ui/WeatherCardFragment.kt
@@ -0,0 +1,32 @@
+package ru.dmkalvan.weatherapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.dmkalvan.weatherapp.R
+
+class WeatherCardFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather_card, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+                WeatherCardFragment().apply {
+                    arguments = Bundle().apply {
+                    }
+                }
+    }
+}
