e1f91ec9f23a2880cf6ed27320b6c89f0150d3eb
Lokni <dmitrikalvan@gmail.com>
2021-03-02 17:00:50 +0200

    add daily recyclerView

--- AndroidSDK/Kotlin/Lesson_1/app/src/main/java/ru/dmkalvan/weatherapp/data/Weather.kt
+++ AndroidSDK/Kotlin/Lesson_1/app/src/main/java/ru/dmkalvan/weatherapp/data/Weather.kt
@@ -1,21 +1,21 @@
 package ru.dmkalvan.weatherapp.data
 
 data class Weather(
-        var city: City = getDefaultCity(),
-        var weather: String? = "Clear",
-        var temperature: Int? = 25,
-        var minimalTemp: Int? = 23,
-        var maximalTemp: Int? = 27,
-        var sunrise: Double? = 5.45,
-        var sunset: Double? = 22.30,
-        var chanceOfPrecipitation: Int? = 0,
-        var feelsLike: Int? = 26,
-        var humidity: Int? = 15,
-        var wind: String? = "SE",
-        var precipitation: Double? = 0.0,
-        var pressure: Int? = 1005,
-        var visibility: Int? = 15,
-        var uvIndex: Int? = 1
+        val city: City = getDefaultCity(),
+        val weather: String? = "Clear",
+        val temperature: Int? = 25,
+        val minimalTemp: Int? = 23,
+        val maximalTemp: Int? = 27,
+        val sunrise: Double? = 5.45,
+        val sunset: Double? = 22.30,
+        val chanceOfPrecipitation: Int? = 0,
+        val feelsLike: Int? = 26,
+        val humidity: Int? = 15,
+        val wind: String? = "SE",
+        val precipitation: Double? = 0.0,
+        val pressure: Int? = 1005,
+        val visibility: Int? = 15,
+        val uvIndex: Int? = 1
 )
 
 fun getDefaultCity() = City("Mountain View", 37.386051, -122.083855)
