79f86aecc104670724e947ee2907e51f84b0a7cd
Lokni <dmitrikalvan@gmail.com>
2021-03-14 14:00:40 +0200

    Application have internet connections

--- AndroidSDK/Kotlin/Weatherforstudying/app/src/main/java/ru/dmkalvan/weatherforstudying/ui/MainActivity.kt
+++ AndroidSDK/Kotlin/Weatherforstudying/app/src/main/java/ru/dmkalvan/weatherforstudying/ui/MainActivity.kt
@@ -3,10 +3,20 @@ package ru.dmkalvan.weatherforstudying.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.dmkalvan.weatherforstudying.R
+import ru.dmkalvan.weatherforstudying.databinding.ActivityMainBinding
+import ru.dmkalvan.weatherforstudying.ui.citylist.CityListFragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (savedInstanceState == null){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, CityListFragment.newInstance())
+                .commitAllowingStateLoss()
+        }
     }
 }
