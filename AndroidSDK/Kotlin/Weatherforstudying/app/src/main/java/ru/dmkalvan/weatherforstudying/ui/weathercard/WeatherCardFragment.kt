4f796c89ad01454c229501a5ef095f64367eb7ec
Lokni <dmitrikalvan@gmail.com>
2021-03-16 12:50:54 +0200

    Add Service

--- AndroidSDK/Kotlin/Weatherforstudying/app/src/main/java/ru/dmkalvan/weatherforstudying/ui/weathercard/WeatherCardFragment.kt
+++ AndroidSDK/Kotlin/Weatherforstudying/app/src/main/java/ru/dmkalvan/weatherforstudying/ui/weathercard/WeatherCardFragment.kt
@@ -1,5 +1,8 @@
 package ru.dmkalvan.weatherforstudying.ui.weathercard
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,10 +11,25 @@ import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import ru.dmkalvan.weatherforstudying.R
+import ru.dmkalvan.weatherforstudying.data.FactDTO
 import ru.dmkalvan.weatherforstudying.data.Weather
 import ru.dmkalvan.weatherforstudying.data.WeatherDTO
 import ru.dmkalvan.weatherforstudying.databinding.FragmentWeatherCardBinding
-
+const val DETAILS_INTENT_FILTER = "DETAILS INTENT FILTER"
+const val DETAILS_LOAD_RESULT_EXTRA = "LOAD RESULT"
+const val DETAILS_INTENT_EMPTY_EXTRA = "INTENT IS EMPTY"
+const val DETAILS_DATA_EMPTY_EXTRA = "DATA IS EMPTY"
+const val DETAILS_RESPONSE_EMPTY_EXTRA = "RESPONSE IS EMPTY"
+const val DETAILS_REQUEST_ERROR_EXTRA = "REQUEST ERROR"
+const val DETAILS_REQUEST_ERROR_MESSAGE_EXTRA = "REQUEST ERROR MESSAGE"
+const val DETAILS_URL_MALFORMED_EXTRA = "URL MALFORMED"
+const val DETAILS_RESPONSE_SUCCESS_EXTRA = "RESPONSE SUCCESS"
+const val DETAILS_TEMP_EXTRA = "TEMPERATURE"
+const val DETAILS_FEELS_LIKE_EXTRA = "FEELS LIKE"
+const val DETAILS_CONDITION_EXTRA = "CONDITION"
+private const val TEMP_INVALID = -100
+private const val FEELS_LIKE_INVALID = -100
+private const val PROCESS_ERROR = "Process error"
 
 class WeatherCardFragment : Fragment() {
 
@@ -29,6 +47,51 @@ class WeatherCardFragment : Fragment() {
             }
 
         }
+    private val loadResultsReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            when (intent.getStringExtra(DETAILS_LOAD_RESULT_EXTRA)) {
+                DETAILS_INTENT_EMPTY_EXTRA -> TODO(PROCESS_ERROR)
+                DETAILS_DATA_EMPTY_EXTRA -> TODO(PROCESS_ERROR)
+                DETAILS_RESPONSE_EMPTY_EXTRA -> TODO(PROCESS_ERROR)
+                DETAILS_REQUEST_ERROR_EXTRA -> TODO(PROCESS_ERROR)
+                DETAILS_REQUEST_ERROR_MESSAGE_EXTRA -> TODO(PROCESS_ERROR)
+                DETAILS_URL_MALFORMED_EXTRA -> TODO(PROCESS_ERROR)
+                DETAILS_RESPONSE_SUCCESS_EXTRA -> renderData(
+                    WeatherDTO(
+                        FactDTO(
+                            intent.getIntExtra(
+                                DETAILS_TEMP_EXTRA, TEMP_INVALID
+                            ),
+                            intent.getIntExtra(DETAILS_FEELS_LIKE_EXTRA, FEELS_LIKE_INVALID),
+                            intent.getStringExtra(
+                                DETAILS_CONDITION_EXTRA
+                            )
+                        )
+                    )
+                )
+                else -> TODO(PROCESS_ERROR)
+            }
+        }
+    }
+
+    private fun renderData(weatherDTO: WeatherDTO) {
+        binding.mainView.visibility = View.VISIBLE
+        binding.loadingLayout.visibility = View.GONE
+
+        val fact = weatherDTO.fact
+        val temp = fact!!.temperature
+        val feelsLike = fact.feelsLike
+        val condition = fact.conditions
+        if (temp == TEMP_INVALID || feelsLike == FEELS_LIKE_INVALID || condition == null) {
+            TODO(PROCESS_ERROR)
+        } else {
+            val city = weatherBundle.city
+            binding.cityName.text = city.city
+            binding.currentTemperature.text = String.format(getString(R.string.current_temperature), temp)
+            binding.feelsLike.text = String.format(getString(R.string.current_temperature), feelsLike)
+            binding.weatherConditions.text = condition
+        }
+    }
 
     private fun displayWeather(weatherDTO: WeatherDTO) {
         with(binding) {
