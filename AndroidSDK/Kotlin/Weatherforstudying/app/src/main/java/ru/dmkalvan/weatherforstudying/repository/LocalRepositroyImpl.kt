db545c41641b8aa56082d803094ddb5871692994
Lokni <dmitrikalvan@gmail.com>
2021-03-19 12:05:41 +0200

    Room is created

--- AndroidSDK/Kotlin/Weatherforstudying/app/src/main/java/ru/dmkalvan/weatherforstudying/repository/LocalRepositroyImpl.kt
+++ AndroidSDK/Kotlin/Weatherforstudying/app/src/main/java/ru/dmkalvan/weatherforstudying/repository/LocalRepositroyImpl.kt
@@ -1,6 +1,7 @@
 package ru.dmkalvan.weatherforstudying.repository
 
 import ru.dmkalvan.weatherforstudying.model.Weather
+import ru.dmkalvan.weatherforstudying.room.HistoryDao
 
 class LocalRepositroyImpl(private val localDataSource: HistoryDao): LocalRepository {
     override fun getAllHistory(): List<Weather> {
---
+++ AndroidSDK/Kotlin/Weatherforstudying/app/src/main/java/ru/dmkalvan/weatherforstudying/utils/DataUtils.kt
@@ -0,0 +1,2 @@
+package ru.dmkalvan.weatherforstudying.utils
+
