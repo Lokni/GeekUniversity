532a2e82e031c8b389d2f4b3142980ce56e41b03
Lokni <dmitrikalvan@gmail.com>
2021-04-17 16:59:38 +0300

    Add class helper for equilateral images and some icons

--- AndroidSDK/Kotlin/NASAObserver/app/src/main/java/ru/dmkalvan/nasaobserver/ui/MainActivity.kt
+++ AndroidSDK/Kotlin/NASAObserver/app/src/main/java/ru/dmkalvan/nasaobserver/ui/MainActivity.kt
@@ -1,6 +1,7 @@
-package ru.dmkalvan.nasaobserver
+package ru.dmkalvan.nasaobserver.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.dmkalvan.nasaobserver.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
